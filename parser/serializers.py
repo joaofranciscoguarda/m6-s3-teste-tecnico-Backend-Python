9e67a4415370c485edcc69dadd39e7880b413240
João Francisco <joaofrancisco.guarda@gmail.com>
2022-10-07 16:07:02 -0300

    feat: trying to add details to transction_type, failed, not relating id

--- parser/serializers.py
+++ parser/serializers.py
@@ -1,14 +1,26 @@
 from rest_framework import serializers
-from rest_framework.serializers import Serializer, FileField, ModelSerializer
-from .models import Transaction
+from rest_framework.serializers import (FileField, ModelSerializer,
+                                        PrimaryKeyRelatedField, Serializer,
+                                        SlugRelatedField)
 
+from .models import Transaction, Type
+
+
+class TypeSerializer(ModelSerializer):
+  class Meta:
+    model= Type
+    fields = '__all__'
 
 class TransactionSerializer(ModelSerializer):
   class Meta:
     model= Transaction
-    fields= '__all__'
+    fields= ['transaction_type', 'date', 'amount', 'cpf', 'card', 'shop_owner', 'shop_name',]
+
+  # transaction_type = TypeSerializer(read_only=True)
 
 class UploadSerializer(Serializer):
     file_uploaded = FileField()
     class Meta:
         fields = ['file_uploaded']
+
+ 
